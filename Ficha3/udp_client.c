1a7160fd11ff4346baf4ea7583db0b3fea3a310c
Reberpower1 <bmsamendes1@gmail.com>
2025-03-13 11:03:27 +0000

    problemas com o buffer

--- Ficha3/udp_client.c
+++ Ficha3/udp_client.c
@@ -1,17 +1,69 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define BUFLEN 512	// Tamanho do buffer
-#define PORT 9876	// Porto para recepção das mensagens
 
 void erro(char *s) {
-	perror(s);
-	exit(1);
+    perror(s);
+    exit(1);
 }
 
-int main(){
-    
+int main(int argc, char const *argv[]){
+    int recv_len;
 
+    if(argc != 3){
+        printf("Uso: ./udp_client <ip> <port>\n");
+        return 1;
+    }
+
+    struct sockaddr_in si_server;
+
+    int s;
+    socklen_t slen = sizeof(si_server);
+    char buf[BUFLEN];
+
+    // Cria um socket para recepção de pacotes UDP
+    if((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        erro("Erro na criação do socket");
+    }
+
+    // Preenchimento da socket address structure do servidor
+    si_server.sin_family = AF_INET;
+    si_server.sin_port = htons(atoi(argv[2]));
+    if(inet_aton(argv[1], &si_server.sin_addr) == 0){
+        erro("Erro no inet_aton");
+    }
+
+    while(1){
+        fgets(buf, BUFLEN, stdin);
+        buf[strcspn(buf, "\n")] = '\0';
+        // "SAIR" para terminar a ligação
+        if(strcmp(buf, "SAIR") == 0){
+            // Envia a mensagem para o servidor
+            if(sendto(s, buf, strlen(buf), 0, (struct sockaddr *) &si_server, slen) == -1){
+                erro("Erro no sendto");
+            }
+            break;
+        }
+
+        // Envia a mensagem para o servidor
+        if(sendto(s, buf, strlen(buf), 0, (struct sockaddr *) &si_server, slen) == -1){
+            erro("Erro no sendto");
+        }
+
+	    if((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_server, (socklen_t *)&slen)) == -1) {
+            erro("Erro no recvfrom");
+        }
+
+        // Para ignorar o restante conteúdo (anterior do buffer)
+        buf[recv_len]='\0';
+        printf("%s\n", buf);
+    } 
+    
+    close(s);
     return 0;
 }
--- Ficha3/udp_server.c
+++ Ficha3/udp_server.c
@@ -6,18 +6,73 @@
 #include <unistd.h>
 
 #define BUFLEN 512	// Tamanho do buffer
-#define PORT 9876	// Porto para recepção das mensagens
+#define PORT 9875	// Porto para recepção das mensagens
 
 void erro(char *s) {
 	perror(s);
 	exit(1);
 }
 
+char hex[20];
+char bin[BUFLEN];
+
+char* conv_hex(int num){
+	int index = 0;
+	int tmp;
+	while(num > 0){
+		tmp = num % 16;
+
+		if(tmp < 10){
+			hex[index] = tmp + '0';
+		}
+		else{
+			hex[index] = tmp + 'A' - 10;
+		}
+		index++;
+		num = num / 16;
+	}
+	hex[index] = '\0';
+	for(int i = 0; i < index / 2; i++){
+		char temp = hex[i];
+		hex[i] = hex[index - 1 - i];
+		hex[index - 1 - i] = temp;
+	}
+	return hex;
+}
+
+char* conv_bin(int num){
+	int index = 0;
+	int bit_count = 0;
+	while(num > 0){
+		if(bit_count == 4){
+			bin[index++] = ' ';
+			bit_count = 0;
+		}
+		bin[index++] = (num%2) + '0';
+		num = num / 2;
+		bit_count++;
+	}
+	if(bit_count != 4){
+		while(bit_count < 4){
+			bin[index++] = '0';
+			bit_count++;
+		}
+	}
+	bin[index] = '\0';
+	for(int i = 0; i < index / 2; i++){
+		char temp = bin[i];
+		bin[i] = bin[index - 1 - i];
+		bin[index - 1 - i] = temp;
+	}
+	return bin;
+}
+
 int main(void) {
-  struct sockaddr_in si_minha, si_outra;
+
+  	struct sockaddr_in si_server, si_client;
 
 	int s,recv_len;
-	socklen_t slen = sizeof(si_outra);
+	socklen_t slen = sizeof(si_client);
 	char buf[BUFLEN];
 
 	// Cria um socket para recepção de pacotes UDP
@@ -26,25 +81,32 @@ int main(void) {
 	}
 
   // Preenchimento da socket address structure
-	si_minha.sin_family = AF_INET;
-	si_minha.sin_port = htons(PORT);
-	si_minha.sin_addr.s_addr = htonl(INADDR_ANY);
+	si_server.sin_family = AF_INET;
+	si_server.sin_port = htons(PORT);
+	si_server.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	// Associa o socket à informação de endereço
-	if(bind(s,(struct sockaddr*)&si_minha, sizeof(si_minha)) == -1) {
+	if(bind(s,(struct sockaddr*)&si_server, sizeof(si_server)) == -1) {
 		erro("Erro no bind");
 	}
-
-	// Espera recepção de mensagem (a chamada é bloqueante)
-	if((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_outra, (socklen_t *)&slen)) == -1) {
-	  erro("Erro no recvfrom");
+	while(1){
+		// Espera recepção de mensagem (a chamada é bloqueante)
+		if((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_client, (socklen_t *)&slen)) == -1) {
+			erro("Erro no recvfrom");
+		}
+		// Para ignorar o restante conteúdo (anterior do buffer)
+		buf[strcspn(buf, "\n")] = '\0';
+		if(strcmp(buf, "SAIR") == 0){
+			break;
+		}
+	
+		printf("Recebi uma mensagem do sistema com o endereço %s e o porto %d\n", inet_ntoa(si_client.sin_addr), ntohs(si_client.sin_port));
+		char response[BUFLEN];
+		printf("Buffer recebido: %d\n", atoi(buf));
+		sprintf(response, "Número em Binário: %s\nNúmero em hexadecimal: 0x%s\n", conv_bin(atoi(buf)), conv_hex(atoi(buf)));
+		printf("Resposta: %s\n", response);
+		sendto(s, response, strlen(response), 0, (struct sockaddr *) &si_client, slen);
 	}
-	// Para ignorar o restante conteúdo (anterior do buffer)
-	buf[recv_len]='\0';
-
-	printf("Recebi uma mensagem do sistema com o endereço %s e o porto %d\n", inet_ntoa(si_outra.sin_addr), ntohs(si_outra.sin_port));
-	printf("Conteúdo da mensagem: %s\n" , buf);
-
 	// Fecha socket e termina programa
 	close(s);
 	return 0;
